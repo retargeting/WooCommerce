962140fb21a560f00fbf83ac680b19c0700dadda
Carol <pelucaroltheo@gmail.com>
2018-02-26 15:47:44 +0200

    Fix prices for variable products

--- classes/class-wc-retargeting-tracking.php
+++ classes/class-wc-retargeting-tracking.php
@@ -908,9 +908,15 @@ class WC_Integration_Retargeting_Tracking extends WC_Integration
         $min_price = current($prices['sale_price']);
         $max_price = end($prices['regular_price']);
         $price = $min_price !== $max_price ? $max_price : $min_price;
-        $specialPrice = $min_price !== $max_price ? $min_price : 0;
+        $specialPrice = 0;
+
+        if ($product->is_on_sale()) {
+            $specialPrice = $product->get_sale_price();
+        }
+
         $price = wc_get_price_including_tax($product, array('price' => $price));
         $specialPrice = wc_get_price_including_tax($product, array('price' => $specialPrice));
+
         return array(
             (!empty($price) ? $price : 0),
             (!empty($specialPrice) ? $specialPrice : 0)
@@ -930,6 +936,7 @@ class WC_Integration_Retargeting_Tracking extends WC_Integration
         $specialPrice = (!empty($getSpecialPrice) ? $getSpecialPrice : 0);
         $price = wc_get_price_including_tax($product, array('price' => $price));
         $specialPrice = wc_get_price_including_tax($product, array('price' => $specialPrice));
+
         return array(
             (!empty($price) ? $price : 0),
             (!empty($specialPrice) ? $specialPrice : 0)
